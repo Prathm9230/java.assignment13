a90820e0afe5230e5ee1f729198ab3f4395ac59f
Prathm <jawahireprathmesh@gmail.com>
2023-07-08 22:36:04 +0530

    Refactor all three UC's

--- src/com/bridgelabz/minmax/FindMax.java
+++ src/com/bridgelabz/minmax/FindMax.java
@@ -33,4 +33,14 @@ public class FindMax{
             System.out.println("num3 is largest:" + str3);
         }
     }
+
+    //UC-4:refactor
+    public <T extends Comparable<T>> void findMax(T num1, T num2, T num3) {
+        if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0) {
+            System.out.println(" num1 is largest ");
+        } else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0) {
+            System.out.println(" num2 is largest");
+        } else
+            System.out.println(" num3 is largest");
+    }
 }
