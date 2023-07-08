f56d64cb55efa979b066442cb5be7deeb94eb2b8
Prathm <jawahireprathmesh@gmail.com>
2023-07-08 22:02:20 +0530

    Findout maximum between three Floats

--- src/com/bridgelabz/minmax/FindMax.java
+++ src/com/bridgelabz/minmax/FindMax.java
@@ -11,4 +11,15 @@ public class FindMax{
             System.out.println("num3 is largest:" + num3);
         }
     }
+    //UC-2:To find out maximum between three Floats.
+    public static void findMaxFloat(Float num1, Float num2, Float num3) {
+
+        if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0) {
+            System.out.println("num1 is largest:" + num1);
+        } else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0) {
+            System.out.println("num2 is largest:" + num2);
+        } else {
+            System.out.println("num3 is largest:" + num3);
+        }
+    }
 }
