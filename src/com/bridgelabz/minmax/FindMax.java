86a42528767e4737478576ebc35e3b52e98e5e45
Prathm <jawahireprathmesh@gmail.com>
2023-07-08 23:15:46 +0530

    Added printmax method

--- src/com/bridgelabz/minmax/FindMax.java
+++ src/com/bridgelabz/minmax/FindMax.java
@@ -5,25 +5,34 @@ public class FindMax<T extends Comparable<T>>{
     T num2;
     T num3;
 
-    public static <T extends Comparable<T>> void testMaximum(T num1, T num2, T num3) {
+    public FindMax(T num1, T num2, T num3) {
+    }
+
+    public static <T extends Comparable<T>> T testMaximum(T num1, T num2, T num3) {
         if(num1.compareTo(num2) >= 0 && num1.compareTo(num3) >= 0) {
             System.out.println(" num1 is largest ");
+            return num1;
         }
         else if(num2.compareTo(num1) >= 0 && num2.compareTo(num3) >= 0) {
             System.out.println(" num2 is largest");
+            return num2;
         }
         else
             System.out.println(" num3 is largest");
+        return num3;
     }
 
     public void testMaximum() {
-        testMaximum(this.num1, this.num2, this.num3);
+       T max = testMaximum(this.num1, this.num2, this.num3);
+       printMax(max);
     }
-
-    public FindMax(T num1, T num2, T num3) {
-        super();
+    public void Maximum(T num1, T num2, T num3) {
         this.num1 = num1;
         this.num2 = num2;
         this.num3 = num3;
+    }
+
+    public void printMax(T max) {
+        System.out.println(" max = " + max);
     }
 }
--- src/com/bridgelabz/minmax/TestMaximum.java
+++ src/com/bridgelabz/minmax/TestMaximum.java
@@ -1,4 +1,50 @@
 package com.bridgelabz.minmax;
-
+import java.util.Scanner;
 public class TestMaximum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        FindMax obj;
+
+        while(true) {
+            System.out.println("1.Integers\n 2.Float\n");
+            System.out.print(" Please enter your choice: ");
+            int ch = sc.nextInt();
+            switch(ch)
+            {
+                case 1:
+                {
+                    System.out.print(" Please enter the first number: ");
+                    Integer num1 = sc.nextInt();
+
+                    System.out.print(" Please enter the second number: ");
+                    Integer num2 = sc.nextInt();
+
+                    System.out.print(" Please enter the third number: ");
+                    Integer num3 = sc.nextInt();
+
+                    obj = new FindMax<Integer>(num1,num2,num3);
+                    obj.testMaximum();
+                    break;
+                }
+                case 2:
+                {
+                    System.out.print(" Please enter the first number: ");
+                    Float num1 = sc.nextFloat();
+
+                    System.out.print(" Please enter the second number: ");
+                    Float num2 = sc.nextFloat();
+
+                    System.out.print(" Please enter the third number: ");
+                    Float num3 = sc.nextFloat();
+                    obj = new FindMax<Float>(num1,num2,num3);
+                    obj.testMaximum();
+                    break;
+                }
+                default:
+                    System.out.println("Please enter a valid input");
+            }
+
+        }
+    }
+
 }
