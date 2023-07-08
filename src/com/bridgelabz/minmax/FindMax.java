daf482f757a2bf839c28467a041a002803ad12fd
Prathm <jawahireprathmesh@gmail.com>
2023-07-08 21:54:06 +0530

    Findout Maximum between three Integers

---
+++ src/com/bridgelabz/minmax/FindMax.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.minmax;
+
+public class FindMax{
+    public static void findMaxInt(Integer num1, Integer num2, Integer num3) {
+
+        if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0) {
+            System.out.println("num1 is largest:" + num1);
+        } else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0) {
+            System.out.println("num2 is largest:" + num2);
+        } else {
+            System.out.println("num3 is largest:" + num3);
+        }
+    }
+}
